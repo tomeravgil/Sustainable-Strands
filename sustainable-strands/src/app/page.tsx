cfbf6f619f698a4af0bca9320d2a7aafe2cf7e5b
Haydo10104 <123sciencesnowman@gmail.com>
2024-07-29 23:15:58 -0400

    added more routing

--- sustainable-strands/src/app/page.tsx
+++ sustainable-strands/src/app/page.tsx
@@ -1,10 +1,7 @@
-'use client'
-
-import { useChat } from 'ai/react'
-import Transaction_Data_Card from "./components/ui/transaction_cards";
-import Hemp_Distrobution_Graph from "./components/ui/hemp_distribution_graph";
-import LineGraph from "./Analytics/linegraph"
-import PieGraph from "./Analytics/piegraph";
+// src/app/page.tsx
+"use client"
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
   const router = useRouter();
