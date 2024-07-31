5da4c186403a5b0900b24da6c4987e013216557c
Haydo10104 <123sciencesnowman@gmail.com>
2024-07-31 11:50:19 -0400

    made first changes

--- sustainable-strands/src/app/Reg_Nav/page.tsx
+++ sustainable-strands/src/app/Reg_Nav/page.tsx
@@ -1,14 +1,44 @@
-// pages/Reg_Nav.tsx
-import React from 'react';
-import PdfEditor from '../components/ui/pdfeditor';
-
-const RegNav: React.FC = () => {
+"use client"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {Autocomplete, AutocompleteItem} from "@nextui-org/react";
+export const animals = [
+  {label: "Hemp Grower Liscencing", value: "cat", description: "The second most popular pet in the world"},
+  {label: "Sampling Agent Certification   ", value: "dog", description: "The most popular pet in the world"},
+  {label: "Testing Laboratory Identification", value: "elephant", description: "The largest land animal"}
+];
+export default function LoginForm() {
   return (
-    <div>
-      <h1>Edit and Save PDF</h1>
-      <PdfEditor pdfUrl="../images/sample.pdf" />
-    </div>
-  );
-};
-
-export default RegNav;
+    <>
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-xl">Sign Up</CardTitle>
+        <CardDescription>
+          Enter your information to create an account
+        </CardDescription>
+      </CardHeader>
+      <Autocomplete
+      isRequired
+      label="Favorite Animal"
+      defaultItems={animals}
+      placeholder="Search an animal"
+      defaultSelectedKey="cat"
+      className={}
+    >
+      {(item) => <AutocompleteItem key={item.value}>{item.label}</AutocompleteItem>}
+    </Autocomplete>
+      
+    </Card>
+   
+  </>
+  )
+}
